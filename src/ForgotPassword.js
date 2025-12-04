28e5caf7fed98d2852ccf8f7150f459621a4812e
yeswanthvuddagiri <yeswanthyeswanth20@gmail.com>
2025-12-04 13:55:11 +0530

    Update ForgotPassword.js

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -7,7 +7,7 @@ const ForgotPassword = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("https://backend-sc0x.onrender.com/forgot-password", {
+    const res = await fetch("https://career-backend-y8om.onrender.com/forgot-password", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email }),
